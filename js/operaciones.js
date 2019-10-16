ebfe13bba3c48b05356f3979a7d75813b4671ce6
davidplaza94 <32297797+davidplaza94@users.noreply.github.com>
2019-10-16 18:44:38 +0200

    Update operaciones.js

--- js/operaciones.js
+++ js/operaciones.js
@@ -4,9 +4,10 @@ var suma=num1+num2;
 var resta=num1-num2;
 var multip=num1*num2;
 var div=num1/num2;
+var mod=num1%num2;
 
-
-document.write(suma);
-document.write(resta);
-document.write(multip);
-document.write(div);
+document.write("Suma:",suma);
+document.write("Resta:",resta);
+document.write("Multplicación:",multip);
+document.write("División:",div);
+document.write("Módulo:",mod);
