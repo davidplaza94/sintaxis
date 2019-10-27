b8ff491f20a6c83b1aa0fb814dcd23bf8b7e5e20
davidplaza94 <32297797+davidplaza94@users.noreply.github.com>
2019-10-27 17:17:38 +0100

    Update media.js

--- js/media.js
+++ js/media.js
@@ -2,8 +2,6 @@ var num1=prompt("Número 1");
 var num2=prompt("Número 2");
 var num3=prompt("Número 3");
 var med=(num1+num2+num3)/3;
-<<<<<<< HEAD
+
 document.write("Media ",med);
-=======
-document.write("Media ",med);
->>>>>>> 245f322031ca9aea5a6e31bb1c215ef5c1ba2c9b
+
